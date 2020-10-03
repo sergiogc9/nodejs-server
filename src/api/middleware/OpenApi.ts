dcaa81080e800f006a835e3f37f48f9239ad007d
Sergio Gómez Cosgaya <sergiogcosgaya@gmail.com>
2020-10-03 19:53:47 +0200

    Added swagger ui docs for api

--- src/api/middleware/OpenApi.ts
+++ src/api/middleware/OpenApi.ts
@@ -1,20 +1,40 @@
 import { Application } from 'express';
 import { OpenApiValidator } from 'express-openapi-validator';
+import fs from 'fs';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'js-yaml';
+import JsonRefs from 'json-refs';
 
 import Log from 'api/middleware/Log';
+
+function yamlContentProcessor(res: any, callback: any) {
+	callback(undefined, YAML.safeLoad(res.text));
+}
+
+const rootYamlPath = './src/api/openapi/openapi.yaml';
 
 class OpenApi {
 	static async mount(_express: Application): Promise<Application> {
 		Log.info('Booting the \'OpenApi\' middleware...');
 
 		await new OpenApiValidator({
-			apiSpec: './src/api/openapi/openapi.yaml',
+			apiSpec: rootYamlPath,
 			validateRequests: true,
 			validateResponses: true,
 			$refParser: {
 				mode: 'dereference'
 			}
 		}).install(_express);
+
+		const rootOpenApiJSON = YAML.load(fs.readFileSync(rootYamlPath).toString());
+		const swaggerDoc = await JsonRefs.resolveRefs(rootOpenApiJSON, {
+			location: rootYamlPath,
+			loaderOptions: {
+				processContent: yamlContentProcessor
+			}
+		});
+
+		_express.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc.resolved));
 
 		return _express;
 	}
