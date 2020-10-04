f0ef1a9836ecd811b191546e68a3e584e50ea48f
Sergio Gómez Cosgaya <sergiogcosgaya@gmail.com>
2020-10-04 18:30:42 +0200

    Fix mongo config

--- src/config/dev.config.ts
+++ src/config/dev.config.ts
@@ -2,7 +2,7 @@ import { Config } from 'config';
 
 const config: Config = {
 	port: 4040,
-	mongoUri: 'mongodb://sergio:Foxcaxaza11@192.168.3.10:27017/fox-home-test?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false',
+	mongoUri: 'mongodb://sergio:pass@192.168.3.10:27017/fox-home-test?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false',
 };
 
 export default config;
