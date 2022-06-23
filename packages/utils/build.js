7af0b74177f38f12d275838753dbf472dcce0599
Sergio G Cosgaya <sergiogcosgaya@gmail.com>
2022-06-23 14:00:01 +0200

    fix: Update some libs versions and fix dependandbot alerts

---
+++ packages/utils/build.js
@@ -0,0 +1,17 @@
+const { buildPackage } = require('@sergiogc9/js-bundle');
+
+const isWatchMode = process.argv.includes('--watch');
+
+const performBuild = async () => {
+	const dynamicInputs = ['Api', 'Cache', 'Log', 'Model', 'Pushover'].map(current => `src/${current}/index.ts`);
+
+	await buildPackage({
+		entryPoint: ['src/index.ts', ...dynamicInputs],
+		esbuildOptions: {
+			platform: 'node'
+		},
+		isWatchMode
+	});
+};
+
+performBuild();
