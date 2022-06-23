6b95c3f9e5579f9355ab9c1519e9880a43e9bf35
Sergio G Cosgaya <sergiogcosgaya@gmail.com>
2022-06-23 14:00:01 +0200

    fix: Update some libs versions and fix dependandbot alerts

--- packages/utils/src/Api/expressAsyncHandler.ts
+++ packages/utils/src/Api/expressAsyncHandler.ts
@@ -3,12 +3,14 @@ import { RequestHandler, ErrorRequestHandler } from 'express';
 type Handler = RequestHandler | ErrorRequestHandler;
 
 // Helper function to wrap route controllers using async / await
-const expressAsyncHandler = (fn: Handler) => (...args: any[]) => {
-	/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */
-	// @ts-ignore
-	const fnReturn = fn(...args);
-	const next = args[args.length - 1];
-	return Promise.resolve(fnReturn).catch(next);
-};
+const expressAsyncHandler =
+	(fn: Handler) =>
+	(...args: any[]) => {
+		/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */
+		// @ts-ignore
+		const fnReturn = fn(...args);
+		const next = args[args.length - 1];
+		return Promise.resolve(fnReturn).catch(next);
+	};
 
 export default expressAsyncHandler;
